d20607045bed6f77782816ec2f45a733556e9605
eternityid <son.nguyen.k2set@gmail.com>
2017-10-25 16:57:02 +0700

    Finished Validation

--- FlashcardAPIs/FlashCard.Domains/Models/FlashCard.cs
+++ FlashcardAPIs/FlashCard.Domains/Models/FlashCard.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson.Serialization.Attributes;
 using Flashcard.Infrastructure.MongoDb.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace FlashCard.Models.Domains
 {
     [BsonIgnoreExtraElements]
     public class FlashCard : AuditableEntityBase, IAggregateRoot
     {
+        [Required(ErrorMessage = "Fill the blank, Please!!")]
+        [StringLength(100, MinimumLength = 5)]
         public string FaceA { get; set; }
 
+        [Required(ErrorMessage = "Fill the blank, Please!!")]
+        [StringLength(300, MinimumLength = 5)]
         public string FaceB { get; set; }
 
+        [Required]
         public string FlashCardCategoryId { get; set; }
 
+        [Required]
         public string FlashCardCategoryName { get; set; }
 
         public string UserEmail { get; set; }
