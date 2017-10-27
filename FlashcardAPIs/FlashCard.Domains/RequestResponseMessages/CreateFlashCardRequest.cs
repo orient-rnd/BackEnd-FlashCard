4adc8f6d4e1f4a9ad0634379c26a0ac3bee3c06c
ORIENTSOFTWARE\nhloc <nguyenhuuloc304@gmail.com>
2017-10-27 10:35:20 +0700

    refactor code

--- FlashcardAPIs/FlashCard.Domains/RequestResponseMessages/CreateFlashCardRequest.cs
+++ FlashcardAPIs/FlashCard.Domains/RequestResponseMessages/CreateFlashCardRequest.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace FlashCard.Domains.RequestResponseMessages
 {
     public class CreateFlashCardRequest
     {
+        [Required(ErrorMessage = "Fill the blank, Please!!")]
+        [StringLength(100, MinimumLength = 5)]
+        public string FaceA { get; set; }
+
+        [Required(ErrorMessage = "Fill the blank, Please!!")]
+        [StringLength(300, MinimumLength = 5)]
+        public string FaceB { get; set; }
+
+        [Required]
+        public string FlashCardCategoryId { get; set; }
+
+        [Required]
+        public string FlashCardCategoryName { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public int ViewNumber { get; set; }
     }
 }
